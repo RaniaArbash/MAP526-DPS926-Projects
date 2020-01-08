60e555c2139a869eeaa2878118fc73da870b30be
RaniaArbash <41278913+RaniaArbash@users.noreply.github.com>
2020-01-08 04:20:18 -0500

    Week1: C# Introduction

---
+++ ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        private string name;
+        private int age;
+
+        public string getName() {
+            return name;
+        }
+        public int getAge() {
+            return age;
+        }
+ 
+        public void setName(string n)
+        {
+            name = n;
+        }
+        public void setAge(int a) {
+            age = a;
+        }
+
+        public void checkAge(int a) {
+            switch (a) {
+                case 20:
+                    Console.WriteLine("The age is : 20 years old");
+                    break;
+                case 25:
+                    Console.WriteLine(" The age is : 25 years old");
+                    break;
+                case 27:
+                    Console.WriteLine("The age is : 27 years old");
+                    break;
+            }
+
+
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static string giveDescription() {
+            return "This is our first static function in C#";
+        }
+
+        private int addNumbers(int num1, int num2) {
+
+            return num1 + num2;
+ 
+        }
+
+        static void Main(string[] args)
+        {
+
+            Person std1 = new Person();
+            Person std2 = new Person();
+            std1.setName("Jone");
+            std2.setName("Roy");
+
+            std1.setAge(25);
+            std2.setAge(27);
+
+           
+
+
+            Console.WriteLine("The first studen name is " + std1.getName() + " and his age is " + std1.getAge());
+
+            Console.WriteLine("The second studen name is " + std2.getName() + " and his age is " + std2.getAge());
+
+            std1.checkAge(std1.getAge());
+            std2.checkAge(std2.getAge());
+
+            Console.WriteLine(Program.giveDescription());
+
+            Program firstObject = new Program();
+           int result = firstObject.addNumbers(3, 4);
+            Console.WriteLine(result);
+
+
+
+           // addNumbers(3, 4);
+
+            Console.WriteLine("Hello C#");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("The age is " + Convert.ToInt32(age));
+
+            Console.WriteLine("The name is " + name);
+            Console.ReadLine();
+
+        }
+    }
+}
